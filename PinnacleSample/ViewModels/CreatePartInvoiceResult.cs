3b1beccc30e36f605c73173be3f0b6d00818b7ea
Chris TC Wong <chris102@gmail.com>
2018-06-03 01:35:06 +0800

    Fixing Test Results

--- PinnacleSample/ViewModels/CreatePartInvoiceResult.cs
+++ PinnacleSample/ViewModels/CreatePartInvoiceResult.cs
@@ -8,5 +8,22 @@
         }
 
         public bool Success { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            var result = false;
+            var AnotherResult = obj as CreatePartInvoiceResult;
+            if(obj != null)
+            {
+                result = this.Success == AnotherResult.Success;
+            }
+            return result; 
+        }
+
+        public override int GetHashCode()
+        {
+            var hashcode = Success.GetHashCode();
+            return hashcode;
+        }
     }
 }
